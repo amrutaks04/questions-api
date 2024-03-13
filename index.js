7da3e936af1fc6764443ac45f4e6013f54758773
amruta.ks <amruta.ks2022cse@sece.ac.in>
2024-03-13 19:48:10 +0530

    questions-api

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const express = require('express')
 const Question=require('./schema.js')
 const bodyParser = require('body-parser')
-
+const cors=require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 
 async function connectToDb() {
